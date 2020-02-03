4e5f9341130e5605d5005d7a65bc47c3381665b3
sine <sineline@gmail.com>
2020-02-03 13:47:36 +0000

    commit

--- resetSetup.sh
+++ resetSetup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo docker stop influxdb grafana telegraf && sudo docker rm influxdb grafana telegraf
+echo "Stopping previous containers..."
+sudo docker stop influxdb grafana telegraf 
+echo "Deleting previous containers..."
+sudo docker rm influxdb grafana telegraf
+echo "Removing previous data..."
+rm grafana/grafana.db
+sudo rm -rf influxdb/data/*
+echo "Launching new containers..."
+/bin/bash -c "sudo docker-compose up -d"
